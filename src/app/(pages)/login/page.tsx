25178c260272d8f893a8edcd8a72d391242d4a25
Leewonno <dldnjssh123@naver.com>
2026-03-18 15:46:50 +0900

    [UPDATE] 로그인 페이지 디자인 수정

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -3,7 +3,9 @@ import { LoginForm } from "@/features";
 export default function Login() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="scroll-m-20 font-extrabold text-xl !mb-4">로그인</h1>
+      <div className="font-bold text-xl tracking-tight mb-6!">
+        <span>로그인</span>
+      </div>
       <LoginForm />
     </div>
   );
--- src/features/login-form/index.tsx
+++ src/features/login-form/index.tsx
@@ -1,16 +1,13 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { login } from "@/app/actions/auth";
-import { Button, Card } from "@/components";
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/shadcn/field";
+import { Button } from "@/components";
+import { Field, FieldGroup } from "@/components/ui/shadcn/field";
 import { Input } from "@/components/ui/shadcn/input";
+import { simpleMessageToast } from "@/lib/utils/common";
 import { useUserStore } from "@/store/useUserStore";
 
 export function LoginForm() {
@@ -21,9 +18,6 @@ export function LoginForm() {
   const [userid, setUserid] = useState<string>("");
   // 비밀번호
   const [password, setPassword] = useState<string>("");
-
-  // 에러
-  const [error, setError] = useState<string | null>(null);
 
   // 아이디
   const handleUseridChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,7 +34,7 @@ export function LoginForm() {
   const handleAction = async (formData: FormData) => {
     const result = await login(formData);
     if (result.error) {
-      setError(result.error);
+      simpleMessageToast("로그인 오류", result.error);
       return;
     }
     if (result.success && result.id) {
@@ -50,37 +44,41 @@ export function LoginForm() {
   };
 
   return (
-    <Card className="p-7 pt-10 pb-10">
-      <form action={handleAction} className="w-100">
-        <FieldGroup>
-          <Field>
-            <FieldLabel htmlFor="input-field-userid">아이디</FieldLabel>
-            <Input
-              id="input-field-userid"
-              type="text"
-              name="userid"
-              value={userid}
-              onChange={handleUseridChange}
-              placeholder="Enter your ID"
-            />
-          </Field>
-          <Field>
-            <FieldLabel htmlFor="input-field-password">비밀번호</FieldLabel>
-            <Input
-              id="input-field-password"
-              type="password"
-              name="password"
-              placeholder="Enter your Password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </Field>
-          <FieldDescription className="text-red-500">{error}</FieldDescription>
-          <Button type="submit" className="w-full cursor-pointer">
-            로그인
-          </Button>
-        </FieldGroup>
-      </form>
-    </Card>
+    <form action={handleAction} className="w-80">
+      <FieldGroup>
+        <Field>
+          <Input
+            id="input-field-userid"
+            type="text"
+            name="userid"
+            value={userid}
+            onChange={handleUseridChange}
+            placeholder="아이디"
+          />
+        </Field>
+        <Field>
+          <Input
+            id="input-field-password"
+            type="password"
+            name="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </Field>
+
+        <Button type="submit" className="w-full cursor-pointer mt-2!">
+          로그인
+        </Button>
+        <div className="text-center mt-3">
+          <Link
+            href="/signup"
+            className="text-sm text-muted-foreground underline"
+          >
+            회원가입
+          </Link>
+        </div>
+      </FieldGroup>
+    </form>
   );
 }
