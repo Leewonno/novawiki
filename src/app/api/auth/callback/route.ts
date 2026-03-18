12719a2f16edbaef68062f7932e4b083eb8fa611
이원노 <105614390+Leewonno@users.noreply.github.com>
2026-03-19 01:04:06 +0900

    Update redirect URL to use site URL from env variable

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -3,6 +3,7 @@ import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
+  const siteUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const code = searchParams.get("code");
 
   if (code) {
@@ -10,5 +11,5 @@ export async function GET(request: Request) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(`${origin}/`);
+  return NextResponse.redirect(`${siteUrl}/`);
 }
